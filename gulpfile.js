c5403662ff80fcd6bc73e05c522004f57dd95a3c
Ahmet Çağlar <arcaglar@gmail.com>
2021-06-27 23:44:17 +0300

    set gulp config on gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,18 @@
-function defaultTask(cb) {
-    cb();
-  }
-  
-  exports.default = defaultTask
+const gulp        = require('gulp');
+const browserSync = require('browser-sync').create();
+const reload      = browserSync.reload;
+
+// Watch scss AND html files, doing different things with each.
+gulp.task('serve', () => {
+
+    // Serve files from the root of this project
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+
+    gulp.watch("*.css").on("change", reload);
+    gulp.watch("*.js").on("change", reload);
+    gulp.watch("*.html").on("change", reload);
+});
